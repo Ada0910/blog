26a2f99e8998e1fa0a91afff46120811f996445c
Ada0910 <1355948107@qq.com>
2020-02-23 16:05:56 +0800

    Redis配置类

--- src/main/java/com/ada/blog/BlogApplication.java
+++ src/main/java/com/ada/blog/BlogApplication.java
@@ -3,10 +3,12 @@ package com.ada.blog;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 
 @MapperScan("com.ada.blog.mapper")
 @SpringBootApplication
+@EnableCaching
 public class BlogApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/ada/blog/config/RedisConfig.java
@@ -0,0 +1,38 @@
+package com.ada.blog.config;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+
+/**
+ * @author Ada
+ * @ClassName :RedisConfig
+ * @date 2020/2/23 14:19
+ * @Description: Redis操作类
+ */
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+
+        //使用Jackson2JsonRedisSerializer来序列化和反序列化redis的value值
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
+
+        RedisTemplate template = new RedisTemplate();
+        template.setConnectionFactory(factory);
+        template.setValueSerializer(jackson2JsonRedisSerializer);
+        template.afterPropertiesSet();
+
+        return template;
+    }
+}
