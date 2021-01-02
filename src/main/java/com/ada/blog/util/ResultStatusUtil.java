f00a9a6aa8e844d02b23b5a78fa5c24df9321316
Ada0910 <1355948107@qq.com>
2021-01-02 19:28:22 +0800

    公共类封装

--- src/main/java/com/ada/blog/util/ResultStatusUtil.java
+++ src/main/java/com/ada/blog/util/ResultStatusUtil.java
@@ -10,14 +10,36 @@ import org.springframework.util.StringUtils;
  */
 public class ResultStatusUtil {
 
-    private static final String DEFAULT_SUCCESS_MESSAGE = "SUCCESS";
-    private static final String DEFAULT_FAIL_MESSAGE = "FAIL";
+    private static final String DEFAULT_SUCCESS_MESSAGE = "成功";
+    private static final String DEFAULT_FAIL_MESSAGE = "服务器内部出现了错误";
     private static final int RESULT_CODE_SUCCESS = 200;
     private static final int RESULT_CODE_SERVER_ERROR = 500;
 
-    /**
-     * 成功
-     */
+    /***
+     * @Description 设置状态和返回信息
+     *
+     * @param code 状态码
+     * @param message 信息
+     * @return com.ada.blog.util.ResultUtil
+     * @Author Ada
+     * @Date 23:54 2021/1/1
+     **/
+    public static ResultUtil setCodeAndMsg(int code, String message,Object data) {
+        ResultUtil result = new ResultUtil();
+        result.setResultCode(code);
+        result.setMessage(message);
+        result.setData(data);
+        return result;
+    }
+
+    /***
+     * @Description 成功状态
+     *
+     * @param
+     * @return com.ada.blog.util.ResultUtil
+     * @Author Ada
+     * @Date 18:21 2021/1/2
+     **/
     public static ResultUtil successResult() {
         ResultUtil result = new ResultUtil();
         result.setMessage(DEFAULT_SUCCESS_MESSAGE);
@@ -25,9 +47,7 @@ public class ResultStatusUtil {
         return result;
     }
 
-    /**
-     * 成功
-     */
+
     public static ResultUtil successResult(String message) {
         ResultUtil result = new ResultUtil();
         result.setResultCode(RESULT_CODE_SUCCESS);
@@ -35,9 +55,7 @@ public class ResultStatusUtil {
         return result;
     }
 
-    /**
-     * 成功
-     */
+
     public static ResultUtil successResult(Object data) {
         ResultUtil result = new ResultUtil();
         result.setResultCode(RESULT_CODE_SUCCESS);
@@ -46,9 +64,30 @@ public class ResultStatusUtil {
         return result;
     }
 
-    /**
-     * 失败
-     */
+    public static ResultUtil successResult(Object data,String message) {
+        ResultUtil result = new ResultUtil();
+        result.setResultCode(RESULT_CODE_SUCCESS);
+        result.setMessage(message);
+        result.setData(data);
+        return result;
+    }
+
+    /***
+     * @Description  失败
+     *
+     * @param
+     * @return com.ada.blog.util.ResultUtil
+     * @Author Ada
+     * @Date 18:06 2021/1/2
+     **/
+    public static ResultUtil failResult() {
+        ResultUtil result = new ResultUtil();
+        result.setMessage(DEFAULT_FAIL_MESSAGE);
+        result.setResultCode(RESULT_CODE_SERVER_ERROR);
+        return result;
+    }
+
+
     public static ResultUtil failResult(String message) {
         ResultUtil result = new ResultUtil();
         result.setResultCode(RESULT_CODE_SERVER_ERROR);
@@ -60,13 +99,14 @@ public class ResultStatusUtil {
         return result;
     }
 
-    /**
-     * 错误
-     */
-    public static ResultUtil errorResult(int code, String message) {
+    public static ResultUtil failResult(String message,Object data) {
         ResultUtil result = new ResultUtil();
-        result.setResultCode(code);
         result.setMessage(message);
+        result.setResultCode(RESULT_CODE_SERVER_ERROR);
+        result.setData(data);
         return result;
     }
+
+
+
 }
